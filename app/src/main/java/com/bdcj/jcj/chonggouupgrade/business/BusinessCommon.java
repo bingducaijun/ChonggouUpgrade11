7646bbc76b87dcb6a792dbb22130e327e2d15444
bingducaijun <bingducaijun1455@163.com>
2017-03-27 17:36:38 +0800

    17-3-27

--- app/src/main/java/com/bdcj/jcj/chonggouupgrade/business/BusinessCommon.java
+++ app/src/main/java/com/bdcj/jcj/chonggouupgrade/business/BusinessCommon.java
@@ -1,8 +1,56 @@
 package com.bdcj.jcj.chonggouupgrade.business;
 
+import com.bdcj.jcj.chonggouupgrade.R;
+import com.bdcj.jcj.chonggouupgrade.common.base.BaseActivity;
+import com.lidroid.xutils.http.RequestParams;
+
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.util.DisplayMetrics;
+
 /**
  * Created by Administrator on 2017/3/25.
  */
 
 public class BusinessCommon {
+    private BaseActivity mContext;
+
+    public BusinessCommon(BaseActivity mContext) {
+        this.mContext = mContext;
+    }
+    /**
+     * 手机唯一标识注册
+     */
+    public void mobileRegister() {
+        //如果成功返回UI层Map类型
+        RequestParams params = new RequestParams();
+        // 初始化全局异常处理类
+        TelephonyManager mTm = (TelephonyManager) mContext.getSystemService(Context.TELEPHONY_SERVICE);
+        DisplayMetrics metric = new DisplayMetrics();
+        // 手机品牌
+        params.addQueryStringParameter("BRAND", android.os.Build.BRAND);
+        // 手机型号
+        params.addQueryStringParameter("MODEL", android.os.Build.MODEL);
+        // 操作系统
+        params.addQueryStringParameter("OS", "Android");
+        // 系统版本
+        params.addQueryStringParameter("OSVERSION", android.os.Build.VERSION.RELEASE);
+        metric = new DisplayMetrics();
+
+        mContext.getWindowManager().getDefaultDisplay().getMetrics(metric);
+        // 屏幕分辨率
+        params.addQueryStringParameter("RESOLUTION", "" + metric.widthPixels + "x" + metric.heightPixels);
+        // 屏幕密度
+        params.addQueryStringParameter("MIDU", metric.density + "");
+        // 设备识别号，手机IMEI号
+        params.addQueryStringParameter("UQID", mTm.getDeviceId());
+        // 系统ID号
+        params.addQueryStringParameter("OSID", android.os.Build.ID);
+
+        params.addQueryStringParameter("TVERSION", mContext.getResources().getString(R.string.versionName));
+        params.addQueryStringParameter("TYPE", "A");
+        params.addQueryStringParameter("VERSION", mContext.getResources().getString(R.string.versionName));
+        send(AffConstans.BUSINESS.COMMON_REGISTER, params, mContext, AffConstans.BUSINESS.TAG_COMMOON_REGISTER, SEND_MAP);
+    }
+
 }
--- app/src/main/java/com/bdcj/jcj/chonggouupgrade/ui/activity/WelcomeActivity.java
+++ app/src/main/java/com/bdcj/jcj/chonggouupgrade/ui/activity/WelcomeActivity.java
@@ -1,17 +1,84 @@
 package com.bdcj.jcj.chonggouupgrade.ui.activity;
 
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.bdcj.jcj.chonggouupgrade.R;
+import com.bdcj.jcj.chonggouupgrade.business.BusinessCommon;
 import com.bdcj.jcj.chonggouupgrade.business.entity.SendToUIEty;
 import com.bdcj.jcj.chonggouupgrade.common.base.BaseActivity;
+import com.lidroid.xutils.ViewUtils;
+import com.lidroid.xutils.util.LogUtils;
+import com.lidroid.xutils.view.annotation.ViewInject;
+import com.tencent.stat.MtaSDkException;
+import com.tencent.stat.StatConfig;
+import com.tencent.stat.StatReportStrategy;
+import com.tencent.stat.StatService;
 
 public class WelcomeActivity extends BaseActivity {
+    private BusinessCommon mBusinessCommon;
+    @ViewInject(R.id.net_again)
+    private TextView mTvBtnNetAgain;
+    @ViewInject(R.id.rlyt_nonetwork)
+    private RelativeLayout mRlyNoNetwork;
+    @ViewInject(R.id.lyt_welcome)
+    private LinearLayout mLytWelcome;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        ViewUtils.inject(this);
+        mBusinessCommon = new BusinessCommon(this);
+        mBusinessCommon.mobileRegister();
+        initMTConfig();
+    }
+
+    /**
+     * 测试分析bug
+     */
+    private void initMTConfig() {
+        boolean IsDebug = false;
+        if (IsDebug)
+            initMTConfig(true);
+        else
+            initMTConfig(false);
+        String appKey = "AKN3552SNZGZ";
+        try {
+            StatService.startStatService(this, appKey, com.tencent.stat.common.StatConstants.VERSION);
+        } catch (MtaSDkException e) {
+            LogUtils.e("error=============>腾讯云分析");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * 腾讯云分析MTA ---根据不同的模式，建议设置的开关状态，可根据实际情况调整，仅供参考。
+     *
+     * @param isDebugMode 根据调试或发布条件，配置对应的MTA配置
+     */
+    private void initMTConfig(boolean isDebugMode) {
+        if (isDebugMode) { // 调试时建议设置的开关状态
+            // 查看MTA日志及上报数据内容
+            StatConfig.setDebugEnable(true);
+            // 禁用MTA对app未处理异常的捕获，方便开发者调试时，及时获知详细错误信息。
+            StatConfig.setAutoExceptionCaught(false);
+            // 关闭wifi下及时上报数据
+            StatConfig.setEnableSmartReporting(false);
+            // 调试时，使用实时发送
+            StatConfig.setStatSendStrategy(StatReportStrategy.INSTANT);
+        } else { // 发布时，建议设置的开关状态，请确保以下开关是否设置合理
+            // 禁止MTA打印日志
+            StatConfig.setDebugEnable(false);
+            // 根据情况，决定是否开启MTA对app未处理异常的捕获
+            StatConfig.setAutoExceptionCaught(true);
+            // 选择默认的上报策略
+            StatConfig.setStatSendStrategy(StatReportStrategy.APP_LAUNCH);
+            // 关闭wifi下及时上报数据
+            StatConfig.setEnableSmartReporting(true);
+        }
     }
 
     @Override
